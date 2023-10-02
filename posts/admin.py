a928a998fcf34d9b945aec59c4a217072e04d581
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-03 01:29:25 +0300

    custmize admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import post
+class postadmin(admin.ModelAdmin):
+    list_display=['title','publish_date']
+    search_fields=['title','content']
 
-
-admin.site.register(post)
+admin.site.register(post,postadmin)
--- posts/models.py
+++ posts/models.py
@@ -19,4 +19,6 @@ class post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=50000)
     publish_date=models.DateTimeField()
-    
+    def __str__(self) :
+        return self.title
+       
