9c332497e7c4df3ceafe1db3dbe21f44f289d070
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-03 00:52:33 +0300

    design post model p1

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+'''
+title
+author
+content
+image
+publish_date
+tags
+'''
+'''
+fields
+html widget
+validation
+
+'''
+class post(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=50000)
+    publish_date=models.DateTimeField()
+    
