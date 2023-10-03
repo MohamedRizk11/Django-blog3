db1722343681ed69de24490a2a79a2f4c54fcc05
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-03 21:38:41 +0300

    add auther panel

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 '''
 title
@@ -16,6 +17,7 @@ validation
 
 '''
 class post(models.Model):
+    auther=models.ForeignKey(User,on_delete=models.CASCADE,related_name='post_auther')
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=50000)
     publish_date=models.DateTimeField()
