0204adead1449e0e01ab7c7b29ff351da71bf340
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-07 18:43:46 +0300

    add edit pst ,add post,delete post using cbv

--- posts/views2.py
+++ posts/views2.py
@@ -10,4 +10,18 @@ class postdetail(generic.DetailView):
     model=post
 
 
-class         
+class ADDpost(generic.CreateView):
+    model=post
+    fields=['auther','title','content','tags','image']       
+    success_url='/blog/'
+
+
+class Editpost(generic.UpdateView):
+    model=post
+    fields=['auther','title','content','tags','image']       
+    success_url='/blog/'  
+    template_name='posts/post_edit.html'  
+
+class deletepost(generic.DeleteView):
+    model=post
+    success_url='/blog/'
--- project/urls.py
+++ project/urls.py
@@ -19,14 +19,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from posts.views import post_list, post_detail,add_post,edit_post,delete_post
-from posts.views2 import postlist,postdetail
+from posts.views2 import postlist,postdetail,ADDpost,Editpost,deletepost
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',postlist.as_view()),
-    path('blog/add',add_post),
-    path('blog/<int:post_id>/edit',edit_post),
+    path('blog/add',ADDpost.as_view()),
     path('blog/<int:pk>',postdetail.as_view()),
-    path('blog/<int:post_id>/delete',delete_post),
+    path('blog/<int:pk>/edit',Editpost.as_view()),
+    path('blog/<int:pk>/delete',deletepost.as_view()),
 ]
 urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
